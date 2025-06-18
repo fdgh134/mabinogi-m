e2f7fc16e84a6cfe43efac2dfa305a5d8d79d326
YoomJJu <fdgh134@naver.com>
2025-06-18 16:43:29 +0900

    remove: edit nickname

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,8 @@
 import { useAuthStore } from "../../hooks/useAuthStore";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../lib/firebase";
-import { signOut, updateProfile } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import DarkModeToggle from "../darkmode/DarkModeToggle";
 import CharacterPresetSelector from "./CharacterPresetSelector ";
 
@@ -10,8 +10,6 @@ export default function Header() {
   const user = useAuthStore((state) => state.user);
   const setUser = useAuthStore((state) => state.setUser);
   const navigate = useNavigate();
-  const [editing, setEditing] = useState(false);
-  const [nickname, setNickname] = useState(user?.displayName ?? "");
 
   useEffect(() => {
     if (!user) {
@@ -25,41 +23,13 @@ export default function Header() {
     navigate("/");
   };
 
-  const handleNameSave = async () => {
-    if (!auth.currentUser) return;
-    await updateProfile(auth.currentUser, { displayName: nickname });
-    setUser(auth.currentUser);
-    setEditing(false);
-  }
-
   return (
     <div>
       <div className="flex flex-row align-middle justify-between p-4 lg:p-10 bg-white text-black dark:bg-gray-900 dark:text-white">
         <div className="flex flex-col gap-2">
-          {editing ? (
-            <div className="flex gap-2 items-center">
-              <input
-                aria-label="닉네임 입력"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
-                className="border px-2 py-1 rounded darg:bg-slate-800"
-              />
-              <button onClick={handleNameSave} className="text-sm text-blue-500 hover:underline">
-                저장
-              </button>
-              <button onClick={() => setEditing(false)} className="text-sm text-gray-400 hover:underline">
-                취소
-              </button>
-            </div>
-          ) : (
-            <h1 className="text-xl lg:text-2xl">
-              환영합니다, {user?.displayName}님!
-              <button onClick={() => setEditing(true)} className="ml-2 text-sm text-gray-400 hover:underline">
-                ✏️
-              </button>
-            </h1>
-          )}
-          
+          <h1 className="text-xl lg:text-2xl">
+            환영합니다, {user?.displayName}님!
+          </h1>
         </div>
         <div>
           <div>
